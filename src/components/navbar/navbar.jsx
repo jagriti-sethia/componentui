edddcc278c1da803f864a4b6e2dc122b084be7dd
jagriti-sethia <jain99jagriti@gmail.com>
2023-11-21 14:36:30 +0530

    update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { FaGithub } from "react-icons/fa";
 import "./navbar.css";
 // import logo from "../../assets/sigma.png";
-import { useNavigate } from "react-router-dom";
+import {Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const navigate = useNavigate();
